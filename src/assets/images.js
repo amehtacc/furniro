a0ea6f1bb384e939bc2f3fe566f412b49c7fe740
Aryan Mehta <mehtaaryan2001@gmail.com>
2025-12-22 23:38:59 +0530

    🎨 import assets and created export file for easy access

---
+++ src/assets/images.js
@@ -0,0 +1,68 @@
+// Import home page images
+import Account_Alert from "./homePage/account-alert-outline.png";
+import Bedroom_1 from "./homePage/bedroom_1.jpg";
+import Bedroom_2 from "./homePage/bedroom_2.jpg";
+import Dining_1 from "./homePage/dining_1.jpg";
+import Dining_2 from "./homePage/dining_2.jpg";
+import Heart_Icon from "./homePage/heart_icon.png";
+import Heart_Wishlist_Icon from "./homePage/heart_wishlist_icon.png";
+import Hero_Image from "./homePage/hero_image.jpg";
+import Living_1 from "./homePage/living_1.jpg";
+import Living_2 from "./homePage/living_2.jpg";
+import Logo_Full from "./homePage/logo_full.png";
+import Logo from "./homePage/logo.png";
+import Rectangle_1 from "./homePage/rectangle_1.jpg";
+import Rectangle_2 from "./homePage/rectangle_2.jpg";
+import Rectangle_3 from "./homePage/rectangle_3.jpg";
+import Rectangle_4 from "./homePage/rectangle_4.jpg";
+import Rectangle_5 from "./homePage/rectangle_5.jpg";
+import Rectangle_6 from "./homePage/rectangle_6.jpg";
+import Rectangle_7 from "./homePage/rectangle_7.jpg";
+import Rectangle_8 from "./homePage/rectangle_8.jpg";
+import Rectangle_9 from "./homePage/rectangle_9.jpg";
+import Search_Icon from "./homePage/search_icon.png";
+import Share_Icon from "./homePage/share_icon.png";
+import Shopping_Cart_Icon from "./homePage/shopping-cart-icon.png";
+
+// Import shop page images
+import Customer_Support from "./shopPage/customer-support.png";
+import Guarantee from "./shopPage/guarantee.png";
+import Shipping from "./shopPage/shipping.png";
+import Shop_Hero_Image from "./shopPage/shop_hero_image.jpg";
+import Trophy from "./shopPage/trophy.png";
+
+// Import cart page images
+import Delete_Icon from "./cartPage/delete_icon.png";
+
+export {
+  Account_Alert,
+  Bedroom_1,
+  Bedroom_2,
+  Dining_1,
+  Dining_2,
+  Heart_Icon,
+  Heart_Wishlist_Icon,
+  Hero_Image,
+  Living_1,
+  Living_2,
+  Logo_Full,
+  Logo,
+  Rectangle_1,
+  Rectangle_2,
+  Rectangle_3,
+  Rectangle_4,
+  Rectangle_5,
+  Rectangle_6,
+  Rectangle_7,
+  Rectangle_8,
+  Rectangle_9,
+  Search_Icon,
+  Share_Icon,
+  Shopping_Cart_Icon,
+  Customer_Support,
+  Guarantee,
+  Shipping,
+  Shop_Hero_Image,
+  Trophy,
+  Delete_Icon,
+};
